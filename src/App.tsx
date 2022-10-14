752ab16da5cfa2ab4a0ef606080772ecd15242b1
Elaine Delgado <delgado.elaine03@gmail.com>
2022-10-14 14:24:38 -0300

    adicionando arquivo Logo.png

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './styles/global.css'
 import {Text} from './components/Text'
+import { Signin } from './Pages/Signin'
 
 export const App = () => {
 
   return (
-      <Text >
-        Olaaaa
-      </Text>
+      <Signin />
   )
 }
--- src/Pages/Signin.stories.tsx
+++ src/Pages/Signin.stories.tsx
@@ -8,16 +8,16 @@ export default {
   argTypes: {},
 } as Meta
 
-// export const Default: StoryObj = {
-//   play: async ({ canvasElement }) => {
-//     const canvas =within(canvasElement)
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
 
-//     userEvent.type(canvas.getByPlaceholderText('example.email.com'), 'delgado.elaine@gmail.com')
-//     userEvent.type(canvas.getByPlaceholderText('********'), '12345678')
-//     userEvent.click(canvas.getByRole('button'))
+    userEvent.type(canvas.getByPlaceholderText('example.email.com'), 'delgado.elaine@gmail.com')
+    userEvent.type(canvas.getByPlaceholderText('********'), '12345678')
+    userEvent.click(canvas.getByRole('button'))
 
-//      await waitFor(() => {
-//        return expect(canvas.getByText('Login realizado')).toBeInTheDocument()
-//     })   
-//   }
-// }
+    //  await waitFor(() => {
+    //    return expect(canvas.getByText('Login realizado')).toBeInTheDocument()
+    // })   
+  }
+}
